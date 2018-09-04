6cfabf21f7db9279d8bbf9a02b9dc905af923273
Norton Kelly-Boxall <norton.kellyboxall@hdr.qut.edu.au>
2018-09-04 14:08:37 +1000

    name change fix

--- scripts/update_setupbash.sh
+++ scripts/update_setupbash.sh
@@ -4,7 +4,7 @@
 ## exectution. Don't add variables or definitions here. Use set_env_gummi.
 
 #echo ${PWD}
-echo "** Updating devel/setup.bash to contain ROS_GUMMI_EE enviroment parameter."
+echo "** Updating devel/setup.bash to contain ROS_GUMMI_HEAD enviroment parameter."
 echo "## Automatically added by gummi_head_XXX package" >> ../../devel/setup.bash
 echo "$(tail -n +2 scripts/set_env_gummi.sh)"   >> ../../devel/setup.bash
 
